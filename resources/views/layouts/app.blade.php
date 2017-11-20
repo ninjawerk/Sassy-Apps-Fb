e2595f2910206ddb3344350648453b262fa4447c
miyurusagarage <miyuru.s@intellogic.lk>
2017-11-20 14:13:48 +0530

    changed fb app id

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
         if (d.getElementById(id)) return;
         js = d.createElement(s);
         js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=415969778585936';
+        js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=259800411212763';
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
 
