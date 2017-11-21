d9e3ca27e111616d24b368bfad8540246221ede3
miyurusagarage <miyuru.s@intellogic.lk>
2017-11-21 23:28:11 +0530

    og:url removed

--- resources/views/splash.blade.php
+++ resources/views/splash.blade.php
@@ -2,7 +2,6 @@
 
 @section('facebook_meta')
     @isset($safeResultId)
-        <meta property="og:url" content="{{env('APP_URL') . '?fbid=' . $app->id}}">
         <meta property="og:image"
               content="{{ json_decode( '{' . $appData .'}',true)['arr'][$safeResultId]['image']  }}">
         <meta property="og:description"
