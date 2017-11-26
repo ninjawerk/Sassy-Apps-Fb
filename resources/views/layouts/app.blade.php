c6971c9b9378ee6946ecef8db26c74ee648b6c8d
intellogic <miyuru.s@intellogic.lk>
2017-11-26 14:19:26 +0530

    Chitka Ads

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    @yield('facebook_meta')
-    <!-- CSRF Token -->
+@yield('facebook_meta')
+<!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="propeller" content="4daae8057b8882da2035b0ea968c8c29" />
+    <meta name="propeller" content="4daae8057b8882da2035b0ea968c8c29"/>
     <link rel="icon" href="/img/sassy.png">
     <title>{{ config('app.name', 'Sassy Apps') }}</title>
 
@@ -22,7 +22,9 @@
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-110090186-1"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+        function gtag() {
+            dataLayer.push(arguments);
+        }
         gtag('js', new Date());
 
         gtag('config', 'UA-110090186-1');
@@ -57,7 +59,7 @@
             <div class="col-md-12 ">
                 <div class="row ">
                     @foreach ($latestApps as $app)
-                        <div class="col-md-3 col-lg-offset-1 col-sm-5 col-xs-12  horizontal-align">
+                        <div class="col-md-4 col-lg-3 col-lg-offset-1 col-sm-6 col-xs-12  horizontal-align">
                             <div class="horizontal-align text-center appblock-tile">
                                 <img width="100%" height="200px" src="{{$app->icon_url}}">
                                 <h2>{{$app->name}}</h2>
@@ -77,7 +79,50 @@
 
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}"></script>
-<script data-cfasync='false' type='text/javascript' src='//p252374.clksite.com/adServe/banners?tid=252374_493277_5&type=footer&size=728x90'></script>
+
+<script type="text/javascript"> var infolinks_pid = 3053277;
+    var infolinks_wsid = 0; </script>
+<script type="text/javascript" src="//resources.infolinks.com/js/infolinks_main.js"></script>
+
+<div id="chitika-ads-1"></div>
+<script type="text/javascript">
+    /* Calculate the width of available ad space */
+    ad = document.getElementById('chitika-ads-1');
+    if (ad.getBoundingClientRect().width) {
+        adWidth = ad.getBoundingClientRect().width; // for modern browsers
+    } else {
+        adWidth = ad.offsetWidth; // for old IE
+    }
+    ( function () {
+        if (window.CHITIKA === undefined) {
+            window.CHITIKA = {'units': []};
+        }
+        if (adWidth >= 728)
+            var unit = {
+                "publisher": "miyurusagarage",
+                "width": 728,
+                "height": 90,
+                "sid": "Chitika Default"
+            }; /* Leaderboard 728x90 */
+        else if (adWidth >= 200)
+            var unit = {
+                "publisher": "miyurusagarage",
+                "width": 320,
+                "height": 50,
+                "sid": "Chitika Default"
+            };
+        var placement_id = window.CHITIKA.units.length;
+        window.CHITIKA.units.push(unit);
+        document.write('<div id="chitikaAdBlock-' + placement_id + '"></div>');
+    }());
+</script>
+<script type="text/javascript" src="//cdn.chitika.net/getads.js" async></script>
+
+<script type="text/javascript">
+    var uid = '182925';
+    var wid = '392751';
+</script>
+<script type="text/javascript" src="//cdn.popcash.net/pop.js"></script>
 
 </body>
 </html>
